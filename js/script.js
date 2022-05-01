7dce1def442fc8875bd7a428c1ce643395adab35
egiam <ezegiampaoli@gmail.com>
2022-05-01 19:14:56 -0300

    update

--- js/script.js
+++ js/script.js
@@ -146,69 +146,69 @@ function scrollActive() {
 }
 window.addEventListener("scroll", scrollActive);
 
-var fields = {};
+// var fields = {};
 
-document.addEventListener("DOMContentLoaded", function() {
-    fields.fullName = document.getElementById("fullName");
-    fields.email = document.getElementById("email");
-    fields.subject = document.getElementById("subject");
-    fields.message = document.getElementById("message");
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     fields.fullName = document.getElementById("fullName");
+//     fields.email = document.getElementById("email");
+//     fields.subject = document.getElementById("subject");
+//     fields.message = document.getElementById("message");
+// });
 
-function isNotEmpty(value) {
-    if (value == null || typeof value == "undefined") return false;
-    return value.length > 0;
-}
+// function isNotEmpty(value) {
+//     if (value == null || typeof value == "undefined") return false;
+//     return value.length > 0;
+// }
 
-function isEmail(email) {
-    let regex =
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
-}
+// function isEmail(email) {
+//     let regex =
+//         /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+//     return regex.test(String(email).toLowerCase());
+// }
 
-function fieldValidation(field, validationFunction) {
-    if (field == null) return false;
+// function fieldValidation(field, validationFunction) {
+//     if (field == null) return false;
 
-    let isFieldValid = validationFunction(field.value);
-    if (!isFieldValid) {
-        field.className = "placeholderRed";
-    } else {
-        field.className = "";
-    }
+//     let isFieldValid = validationFunction(field.value);
+//     if (!isFieldValid) {
+//         field.className = "placeholderRed";
+//     } else {
+//         field.className = "";
+//     }
 
-    return isFieldValid;
-}
+//     return isFieldValid;
+// }
 
-function isValid() {
-    var valid = true;
+// function isValid() {
+//     var valid = true;
 
-    valid &= fieldValidation(fields.fullName, isNotEmpty);
-    valid &= fieldValidation(fields.email, isNotEmpty);
-    valid &= fieldValidation(fields.subject, isNotEmpty);
-    valid &= fieldValidation(fields.message, isNotEmpty);
+//     valid &= fieldValidation(fields.fullName, isNotEmpty);
+//     valid &= fieldValidation(fields.email, isNotEmpty);
+//     valid &= fieldValidation(fields.subject, isNotEmpty);
+//     valid &= fieldValidation(fields.message, isNotEmpty);
 
-    return valid;
-}
+//     return valid;
+// }
 
-class User {
-    constructor(fullName, email, subject, message) {
-        this.fullName = fullName;
-        this.email = email;
-        this.subject = subject;
-        this.message = message;
-    }
-}
+// class User {
+//     constructor(fullName, email, subject, message) {
+//         this.fullName = fullName;
+//         this.email = email;
+//         this.subject = subject;
+//         this.message = message;
+//     }
+// }
 
-function sendContact() {
-    if (isValid()) {
-        let usr = new User(
-            fullName.value,
-            email.value,
-            subject.value,
-            message.value
-        );
-        alert("Thank you for contacting me!");
-    } else {
-        alert("There was an error");
-    }
-}
+// function sendContact() {
+//     if (isValid()) {
+//         let usr = new User(
+//             fullName.value,
+//             email.value,
+//             subject.value,
+//             message.value
+//         );
+//         alert("Thank you for contacting me!");
+//     } else {
+//         alert("There was an error");
+//     }
+// }
